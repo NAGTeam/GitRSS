236ba8b53938355d7c7acea7d3d1b8807d63d79f
Niccolò Cantù <niccolocantu94@hotmail.it>
2014-06-12 00:08:19 +0200

    basic function added
    layout uses brick's deck, xmlHTTPrequest gets data from gitHub and local
    storage saves data. only style change needed for version 1.0

---
+++ js/localStorage.js
@@ -0,0 +1,45 @@
+//LOCAL STORAGE 
+
+//if counter key doesn't exist, create and initialize.
+function init(){
+		
+    if (isNaN(parseInt(localStorage.getItem('counter')))) {
+        localStorage.setItem('counter', '0');
+    }
+	count=parseInt(localStorage.getItem('counter'));
+	console.log(count);
+		for(i=1; i<=count;i++) {
+			if(JSON.parse(localStorage.getItem(i+"")) !== null){
+				oggetto = JSON.parse(localStorage.getItem(i+""));
+				console.log(oggetto);
+			}
+		}
+}
+
+function save(dataJSON){
+	console.log('saving '+ dataJSON);
+	JString=JSON.stringify(dataJSON);
+	count=parseInt(localStorage.getItem('counter'));
+	localStorage.setItem('counter',(++count)+"");
+	localStorage.setItem(count+"", JString);
+	
+}
+
+function getData(section){
+	console.log('getting');
+	count=parseInt(localStorage.getItem('counter'));
+	for(i=1; i<=count;i++) {
+		if(JSON.parse(localStorage.getItem(i+"")) !== null){
+			oggetto = JSON.parse(localStorage.getItem(i+""));
+			console.log(i);
+			if(section == 'history'){
+				if(oggetto['section'] == 'history') 
+					$('#list_his').prepend('<li id='+i+' class="found_his"><a href="#"><b>'+oggetto["user"]+'</b>' +oggetto["repo"]+'</a></li>');
+			}
+			if(section == 'starred'){
+				if(oggetto['section'] == 'starred')
+				$('#star_list').prepend('<li id='+i+' class="found_star"><a href="#"><b>'+oggetto["user"]+'</b>' +oggetto["repo"]+'</a></li>');
+			}
+		}
+	}
+}
--- js/script.js
+++ js/script.js
@@ -1,36 +1,73 @@
+
 $(document).ready(function(){
 	console.log('start');
-	$(document).on('click','button',function(){
+	init();
+	
+	$(document).on('click','.toSearch',function(){
+		goToCard(0);
+	});
+	
+	$(document).on('click','.toStarred',function(){
+		goToCard(2);
+		getData('starred');
+	});
+	
+	$(document).on('click','.toHistory',function(){
+		goToCard(3);
+		getData('history');
+	});
+
+	$(document).on('click','#submit',function(){
 		console.log('click');
-		data=getData();
 		//$('#reslist').empty();
-		request(data);
+		branch=$('input[name=branch]').val();
+		repo=$('input[name=repo]').val();
+		user=$('input[name=user]').val();
+		sendRequest(user,repo,branch);
+	});
+	
+	$(document).on('click','.found_his',function(){
+		console.log($(this).attr('id'));
+		showNumber = parseInt($(this).attr('id'));
+		selected = JSON.parse(localStorage.getItem(showNumber+""));
+		user=selected['user'];
+		repo=selected['repo'];
+		branch=selected['branch'];
+		sendRequest(selected['user'],selected['repo'],selected['branch']);
+	});
+	
+	$(document).on('click','.found_star',function(){
+		console.log($(this).attr('id'));
+		showNumber = parseInt($(this).attr('id'));
+		selected = JSON.parse(localStorage.getItem(showNumber+""));
+		user=selected['user'];
+		repo=selected['repo'];
+		branch=selected['branch'];
+		sendRequest(selected['user'],selected['repo'],selected['branch']);
+	});
+	
+	$(document).on('click','#star_this',function(){
+		faveData={
+			'user':user,
+			'repo':repo,
+			'branch':branch,
+			'section':'starred'
+		};
+		save(faveData);
+		alert('aggiunto ai preferiti');
 	});
 });
-
-
-function getData(){
-	user=$('input[name=user]').val();
-	repo=$('input[name=repo]').val();
-	branch=$('input[name=branch]').val();
 	
-	data={
-		user : user,
-		repo : repo,
-		branch : branch
-	};
-	
-	console.log(data);
-	return data;
-}
-	
-	
-function request(data){
+function sendRequest(user,repo,branch){
 	request=new XMLHttpRequest({mozSystem:true});
-	url= 'http://github.com/'+data.user+'/'+data.repo+'/commits/'+data.branch+'.atom';
+	console.log(repo);
+	$('#repo').empty();
+	$('#repo').append('<h1>'+repo+'</h1>');
+	url= 'http://github.com/'+user+'/'+repo+'/commits/'+branch+'.atom';
 	request.open('GET', url);
-	request.addEventListener('load', function(){
-		if(request.status===200){
+	request.send();
+	request.onreadystatechange=function(){
+		if(request.status===200 && request.readyState==4){
 			console.log(request.responseText);
 				
 			response=request.responseText;
@@ -42,18 +79,42 @@ function request(data){
 			author= $response.find('name');
 			console.log(author);
 			update=$response.find('entry updated');
+			$('#reslist').empty();
+			
 				
 			for(i=0;i<=titolo.length; i++){
-				date=(update[i].textContent).match(/(\d+)\-(\d+)\-(\d+)\w(\d+:\d+):/);
-				year=date[1];
-				month=date[2];
-				day=date[3];
-				hour=date[4];
-				console.log(date);
-				$('#reslist').append('<header>'+day+'-'+month+' '+hour+' by '+author[i].textContent+'</header><p>'+titolo[i].textContent+'</p>');
+				if(update[i] != undefined){
+					date=(update[i].textContent).match(/(\d+)\-(\d+)\-(\d+)\w(\d+:\d+):/);
+					year=date[1];
+					month=date[2];
+					day=date[3];
+					hour=date[4];
+					console.log(date);
+					$('#reslist').append('<header>'+day+'-'+month+' '+hour+' by '+author[i].textContent+'</header><p>'+titolo[i].textContent+'</p>');
+				}else{
+					break;
+				}
 			}
+			goToCard(1);
 		}
-	});
-	request.send();
-	$('.input').val('');
+	};
+	
+	searchData={
+		'user':user,
+		'repo':repo,
+		'branch':branch,
+		'section':'history'
+	};
+	save(searchData);
 }
+
+function goToCard(cardNum){
+	document.querySelector('x-deck').showCard(cardNum);
+	$('input[name=branch]').val("");
+	$('input[name=repo]').val("");
+	$('input[name=user]').val("");
+	$('#list_his').empty();
+	$('#star_list').empty();	
+	
+}
+
